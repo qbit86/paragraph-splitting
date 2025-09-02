960e25bb36adfad1358c8ce81c1eb2755c1579c8
Viktor Ptitselov <qbit86@gmail.com>
2025-09-02 13:14:41 +0300

    Add initial implementation of ParagraphSplitter and project configuration

---
+++ src/ParagraphSplitting.Library/ParagraphSplitter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ParagraphSplitting;
+
+public static class ParagraphSplitter
+{
+    public static IEnumerable<int> SplitParagraph(IReadOnlyList<int> wordWidths, int spaceWidth, int pageWidth)
+    {
+        ArgumentNullException.ThrowIfNull(wordWidths);
+        ArgumentOutOfRangeException.ThrowIfNegative(spaceWidth);
+        ArgumentOutOfRangeException.ThrowIfNegative(pageWidth);
+
+        if (wordWidths.Count is 0)
+            return [];
+
+        List<int> leadingWordIndices = [];
+        SplitParagraph(wordWidths, spaceWidth, pageWidth, leadingWordIndices);
+        var trailingWordIndices = leadingWordIndices.Select(it => it - 1).Skip(1).Append(wordWidths.Count - 1);
+        return trailingWordIndices;
+    }
+
+    private static void SplitParagraph(
+        IReadOnlyList<int> wordWidths, int spaceWidth, int pageWidth, List<int> leadingWordIndices) =>
+        throw new NotImplementedException();
+}
